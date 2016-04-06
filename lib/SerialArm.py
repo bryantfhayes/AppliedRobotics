a482806aa3098cc515d3e75c65d44cc3590338db
Bryant Hayes <BryantfHayes@gmail.com>
2016-04-06 01:53:16 -0700

    Got XYZ movments working.

--- lib/SerialArm.py
+++ lib/SerialArm.py
@@ -1,12 +1,18 @@
 import IKHelper,serial
 
+class Tool(object):
+  def __init__(self,x,y,z):
+    self.x, self.y, self.z = (x,y,z)
+
 # Robotic arm class responsible for sending movement PWM signals to 
 # an Arduino or some microcontroller device which then writes those 
 # values to individual servos. 
 class SerialArm(object):
-  def __init__(self, tibia, femur, port=None):
+  def __init__(self, tibia, femur, tool, port=None, z_offset=0):
     self.x, self.y, self.z, self.r = (1426,1500,1500,1490)
     self.port = port
+    self.tool = tool
+    self.z_offset = z_offset
     if self.port:
     	self._ser = serial.Serial(port,115200,timeout=5) 
     self.ik = IKHelper.IKHelper(tibia, femur)
@@ -27,7 +33,7 @@ class SerialArm(object):
   # Takes either a tuple (X, Y, Z) or individual X, Y, Z args
   def setPosition(self, X, Y=None, Z=None):
     if isinstance(X, tuple) or isinstance(X, list):
-      self.x, self.y, self.z = self.ik.getPWM(X[0], X[1], X[2])
+      self.x, self.y, self.z = self.ik.getPWM(X[0] - self.tool.x, X[1] - self.tool.y, X[2] + self.tool.z - self.z_offset)
     else:
-      self.x, self.y, self.z = self.ik.getPWM(X,Y,Z)
+      self.x, self.y, self.z = self.ik.getPWM(X - self.tool.x, Y - self.tool.y, Z + self.tool.z - self.z_offset)
     self.updateArm()
--- src/main.py
+++ src/main.py
@@ -8,12 +8,13 @@ parser = argparse.ArgumentParser(description='Process command line arguments')
 parser.add_argument("-t", "--testmode", help="Run without serial device attached", action='store_true')
 args = parser.parse_args()
 
+fishing_pole = SerialArm.createTool(2,19,17)
+
 def main():
 	if args.testmode:
-		arm = SerialArm.SerialArm(15.5, 14)
+		arm = SerialArm.SerialArm(15.5, 14, fishing_pole, z_offset=10.5)
 	else:
-    arm = SerialArm.SerialArm(15.5,14,'/dev/cu.usbmodem1411')
-
+    arm = SerialArm.SerialArm(15.5,14, fishing_pole,'/dev/cu.usbmodem1411', z_offset=10.5)
 	while True:	
 		print "Enter XYZ Coordinates (ex. 10,10,10): "
 		coords = raw_input()
